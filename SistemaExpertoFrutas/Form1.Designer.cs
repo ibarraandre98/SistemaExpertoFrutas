aa95ec95df9122ba7c256cda2f45103483116b7c
Andre Ibarra Perez <RayAsm1@hotmail.com>
2020-04-23 15:57:08 -0500

    Commit inicial

--- SistemaExpertoFrutas/Form1.Designer.cs
+++ SistemaExpertoFrutas/Form1.Designer.cs
@@ -28,13 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelFruta = new System.Windows.Forms.Panel();
+            this.cbAcido = new System.Windows.Forms.ComboBox();
+            this.panelOpciones = new System.Windows.Forms.Panel();
+            this.cbForma = new System.Windows.Forms.ComboBox();
+            this.cbColores = new System.Windows.Forms.ComboBox();
+            this.cbPiedra = new System.Windows.Forms.ComboBox();
+            this.cbTallo = new System.Windows.Forms.ComboBox();
+            this.btnAdivinar = new System.Windows.Forms.Button();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.labelFruta = new System.Windows.Forms.Label();
+            this.panelFruta.SuspendLayout();
+            this.panelOpciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelFruta
+            // 
+            this.panelFruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panelFruta.Controls.Add(this.labelFruta);
+            this.panelFruta.Controls.Add(this.pbImagen);
+            this.panelFruta.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFruta.Location = new System.Drawing.Point(0, 0);
+            this.panelFruta.Name = "panelFruta";
+            this.panelFruta.Size = new System.Drawing.Size(800, 374);
+            this.panelFruta.TabIndex = 4;
+            // 
+            // cbAcido
+            // 
+            this.cbAcido.BackColor = System.Drawing.Color.White;
+            this.cbAcido.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAcido.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbAcido.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbAcido.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(54)))));
+            this.cbAcido.FormattingEnabled = true;
+            this.cbAcido.Items.AddRange(new object[] {
+            "ACIDO",
+            "si",
+            "no"});
+            this.cbAcido.Location = new System.Drawing.Point(359, 140);
+            this.cbAcido.Name = "cbAcido";
+            this.cbAcido.Size = new System.Drawing.Size(176, 26);
+            this.cbAcido.TabIndex = 3;
+            // 
+            // panelOpciones
+            // 
+            this.panelOpciones.BackColor = System.Drawing.Color.Lime;
+            this.panelOpciones.Controls.Add(this.btnAdivinar);
+            this.panelOpciones.Controls.Add(this.cbForma);
+            this.panelOpciones.Controls.Add(this.cbColores);
+            this.panelOpciones.Controls.Add(this.cbPiedra);
+            this.panelOpciones.Controls.Add(this.cbAcido);
+            this.panelOpciones.Controls.Add(this.cbTallo);
+            this.panelOpciones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelOpciones.Location = new System.Drawing.Point(0, 374);
+            this.panelOpciones.Name = "panelOpciones";
+            this.panelOpciones.Size = new System.Drawing.Size(800, 219);
+            this.panelOpciones.TabIndex = 5;
+            // 
+            // cbForma
+            // 
+            this.cbForma.BackColor = System.Drawing.Color.White;
+            this.cbForma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbForma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbForma.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbForma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(54)))));
+            this.cbForma.FormattingEnabled = true;
+            this.cbForma.Items.AddRange(new object[] {
+            "FORMAS",
+            "creciente",
+            "esfera",
+            "esfera conica",
+            "esfera aplanada"});
+            this.cbForma.Location = new System.Drawing.Point(359, 12);
+            this.cbForma.Name = "cbForma";
+            this.cbForma.Size = new System.Drawing.Size(176, 26);
+            this.cbForma.TabIndex = 6;
+            // 
+            // cbColores
+            // 
+            this.cbColores.BackColor = System.Drawing.Color.White;
+            this.cbColores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbColores.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbColores.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbColores.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(54)))));
+            this.cbColores.FormattingEnabled = true;
+            this.cbColores.Items.AddRange(new object[] {
+            "COLORES",
+            "amarillo",
+            "verde",
+            "rojo",
+            "morado",
+            "naranja",
+            "durazno"});
+            this.cbColores.Location = new System.Drawing.Point(359, 44);
+            this.cbColores.Name = "cbColores";
+            this.cbColores.Size = new System.Drawing.Size(176, 26);
+            this.cbColores.TabIndex = 5;
+            // 
+            // cbPiedra
+            // 
+            this.cbPiedra.BackColor = System.Drawing.Color.White;
+            this.cbPiedra.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPiedra.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbPiedra.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPiedra.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(54)))));
+            this.cbPiedra.FormattingEnabled = true;
+            this.cbPiedra.Items.AddRange(new object[] {
+            "PIEDRA",
+            "si",
+            "no"});
+            this.cbPiedra.Location = new System.Drawing.Point(359, 76);
+            this.cbPiedra.Name = "cbPiedra";
+            this.cbPiedra.Size = new System.Drawing.Size(176, 26);
+            this.cbPiedra.TabIndex = 4;
+            // 
+            // cbTallo
+            // 
+            this.cbTallo.BackColor = System.Drawing.Color.White;
+            this.cbTallo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTallo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbTallo.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTallo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(106)))), ((int)(((byte)(54)))));
+            this.cbTallo.FormattingEnabled = true;
+            this.cbTallo.Items.AddRange(new object[] {
+            "TALLO",
+            "si",
+            "no"});
+            this.cbTallo.Location = new System.Drawing.Point(359, 108);
+            this.cbTallo.Name = "cbTallo";
+            this.cbTallo.Size = new System.Drawing.Size(176, 26);
+            this.cbTallo.TabIndex = 3;
+            // 
+            // btnAdivinar
+            // 
+            this.btnAdivinar.Location = new System.Drawing.Point(413, 183);
+            this.btnAdivinar.Name = "btnAdivinar";
+            this.btnAdivinar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdivinar.TabIndex = 7;
+            this.btnAdivinar.Text = "Adivinar";
+            this.btnAdivinar.UseVisualStyleBackColor = true;
+            this.btnAdivinar.Click += new System.EventHandler(this.btnAdivinar_Click);
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Location = new System.Drawing.Point(303, 12);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(300, 300);
+            this.pbImagen.TabIndex = 2;
+            this.pbImagen.TabStop = false;
+            // 
+            // labelFruta
+            // 
+            this.labelFruta.AutoSize = true;
+            this.labelFruta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFruta.Location = new System.Drawing.Point(414, 327);
+            this.labelFruta.Name = "labelFruta";
+            this.labelFruta.Size = new System.Drawing.Size(74, 25);
+            this.labelFruta.TabIndex = 3;
+            this.labelFruta.Text = "Fruta:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 593);
+            this.Controls.Add(this.panelOpciones);
+            this.Controls.Add(this.panelFruta);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panelFruta.ResumeLayout(false);
+            this.panelFruta.PerformLayout();
+            this.panelOpciones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panelFruta;
+        private System.Windows.Forms.ComboBox cbAcido;
+        private System.Windows.Forms.Panel panelOpciones;
+        private System.Windows.Forms.ComboBox cbForma;
+        private System.Windows.Forms.ComboBox cbColores;
+        private System.Windows.Forms.ComboBox cbPiedra;
+        private System.Windows.Forms.ComboBox cbTallo;
+        private System.Windows.Forms.Button btnAdivinar;
+        private System.Windows.Forms.Label labelFruta;
+        private System.Windows.Forms.PictureBox pbImagen;
     }
 }
 
--- SistemaExpertoFrutas/Form1.cs
+++ SistemaExpertoFrutas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SbsSW.SwiPlCs;
 
 namespace SistemaExpertoFrutas
 {
@@ -15,6 +16,89 @@ namespace SistemaExpertoFrutas
         public Form1()
         {
             InitializeComponent();
+            firstElementComboBox();
+        }
+
+        private void btnAdivinar_Click(object sender, EventArgs e)
+        {
+            inicializarProlog();
+            adivinar();
+        }
+
+        private void inicializarProlog()
+        {
+            Environment.SetEnvironmentVariable("Path", @"C:\\Program Files (x86)\\swipl\\bin");
+            String[] p = { "-q", "-f", @"frutas.pl" };
+            PlEngine.Initialize(p);
+        }
+
+        private void firstElementComboBox()
+        {
+            cbColores.SelectedIndex = 0;
+            cbForma.SelectedIndex = 0;
+            cbAcido.SelectedIndex = 0;
+            cbTallo.SelectedIndex = 0;
+            cbPiedra.SelectedIndex = 0;
+        }
+
+        public void adivinar()
+        {
+            String query = "es_fruta(" +
+                "FRUTA," +
+                cbForma.Text.ToString() + "," +
+                cbColores.Text.ToString() + "," +
+                cbPiedra.Text.ToString() + "," +
+                cbAcido.Text.ToString() + "," +
+                cbTallo.Text.ToString()+
+                ")";
+
+
+            PlQuery consulta = new PlQuery(query);
+            foreach (PlQueryVariables z in consulta.SolutionVariables)
+            {
+                labelFruta.Text = z["FRUTA"].ToString();
+            }
+            if (labelFruta.Text.Equals("manzana"))
+            {
+                pbImagen.Image = Properties.Resources.manzana;
+            }
+            if (labelFruta.Text.Equals("platano"))
+            {
+                pbImagen.Image = Properties.Resources.platano;
+            }
+            if (labelFruta.Text.Equals("limon"))
+            {
+                pbImagen.Image = Properties.Resources.limon;
+            }
+            if (labelFruta.Text.Equals("pera"))
+            {
+                pbImagen.Image = Properties.Resources.pera;
+            }
+            if (labelFruta.Text.Equals("ciruela"))
+            {
+                pbImagen.Image = Properties.Resources.ciruela;
+            }
+            if (labelFruta.Text.Equals("uva"))
+            {
+                pbImagen.Image = Properties.Resources.uva;
+            }
+            if (labelFruta.Text.Equals("naranja"))
+            {
+                pbImagen.Image = Properties.Resources.naranja;
+            }
+            if (labelFruta.Text.Equals("mandarina"))
+            {
+                pbImagen.Image = Properties.Resources.mandarina;
+            }
+            if (labelFruta.Text.Equals("mango"))
+            {
+                pbImagen.Image = Properties.Resources.mango;
+            }
+            if (labelFruta.Text.Equals("cereza"))
+            {
+                pbImagen.Image = Properties.Resources.cereza;
+            }
+            PlEngine.PlCleanup();
         }
     }
 }
